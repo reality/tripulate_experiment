d005607851bd4ab069fb9c8721ecf092caed6905
Luke Slater <lksr@uhb.nhs.uk>
2020-07-14 19:51:32 +0100

    trainy trainty

--- create_vocabulary.sh
+++ create_vocabulary.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-komenti query --ontology GO -q "<http://www.w3.org/2002/07/owl#Thing>" --expand-synonyms --verbose --out triple_vocab.txt --threads 60 --override-group terms
-komenti query --ontology PATO -q "<http://www.w3.org/2002/07/owl#Thing>" --expand-synonyms --verbose --out triple_vocab.txt --append --threads 60 --override-group terms
-komenti query --ontology RO -q "<http://www.w3.org/2002/07/owl#Thing>" --expand-synonyms --verbose --out triple_vocab.txt --append --threads 60 --override-group terms
-komenti query --ontology CLO -q "<http://www.w3.org/2002/07/owl#Thing>" --expand-synonyms --verbose --out clo_vocab.txt --threads 60 --override-group terms
-komenti query --ontology RO --object-properties --expand-synonyms --verbose --out triple_vocab.txt --append --threads 60 --override-group object-properties
-komenti query --ontology GO --object-properties --expand-synonyms --verbose --out triple_vocab.txt --append --threads 60 --override-group object-properties
-komenti query --ontology CLO --object-properties --expand-synonyms --verbose --out triple_vocab.txt --append --threads 60 --override-group object-properties
-komenti query --ontology PATO --object-properties --expand-synonyms --verbose --out triple_vocab.txt --append --threads 60 --override-group object-properties
+komenti query --ontology GO -q "<http://www.w3.org/2002/07/owl#Thing>" --verbose --out triple_vocab.txt --threads 5 --override-group terms --lemmatise
+komenti query --ontology PATO -q "<http://www.w3.org/2002/07/owl#Thing>" --verbose --out triple_vocab.txt --append --threads 5 --override-group terms --lemmatise
+komenti query --ontology RO -q "<http://www.w3.org/2002/07/owl#Thing>" --verbose --out triple_vocab.txt --append --threads 5 --override-group terms --lemmatise
+komenti query --ontology CLO -q "<http://www.w3.org/2002/07/owl#Thing>" --verbose --out triple_vocab.txt --threads 5 --override-group terms --lemmatise
+komenti query --ontology RO --object-properties --verbose --out triple_vocab.txt --append --threads 5 --override-group object-properties --lemmatise
+komenti query --ontology GO --object-properties --verbose --out triple_vocab.txt --append --threads 5 --override-group object-properties --lemmatise
+komenti query --ontology CLO --object-properties --verbose --out triple_vocab.txt --append --threads 5 --override-group object-properties --lemmatise
+komenti query --ontology PATO --object-properties --verbose --out triple_vocab.txt --append --threads 5 --override-group object-properties --lemmatise
 echo "done, and saved 2 triple_vocab.txt"
